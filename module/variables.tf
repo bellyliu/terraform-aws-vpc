76366d8eade41f2ee4512ecdb37c095d29910e21
dekribellyliu <dekri.liu@ralali.com>
2020-04-14 00:33:07 +0700

    add vpc

---
+++ module/variables.tf
@@ -0,0 +1,19 @@
+variable "profile" {
+  description = "your aws profile"
+}
+
+variable "region" {
+  description = "aws region"
+}
+
+variable "cidr_block" {
+  description = "cidr for your vpc"
+}
+
+variable "enable_dns_hostnames" {
+  description = "enable_dns_hostnames"
+}
+
+variable "enable_dns_support" {
+  description = "enable_dns_support"
+}
